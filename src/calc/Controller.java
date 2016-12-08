0487e6b914ba625f1766487a7dd90c508bbdfd0f
Maxim <entorus@yandex.ru>
2016-12-08 17:19:22 +0900

    -

--- src/calc/Controller.java
+++ src/calc/Controller.java
@@ -45,10 +45,16 @@ public class Controller implements Initializable {
         //TODO add validation
     }
     private String[] netmasks() {
-        String m = "111111111111111111111111111111100000000000000000000000000000000";
-        String[] array = new String[31];
-        for (int i = 0; i < 31; i++) {
-            array[i] = m.substring(i,i+32);
+        String m = "1111111111111111111111111111111100000000000000000000000000000000";
+        String[] array = new String[32];
+        for (int i = 0; i < 32; i++) {
+            int s = 32-i;
+            String string = m.substring(i,i+32);
+            String first=string.substring(0,7);
+            String second=string.substring(8,15);
+            String third=string.substring(16,23);
+            String fourth=string.substring(24,31);
+            array[i] = string + " /" + s;
         }
         return array;
     }
