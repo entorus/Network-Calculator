2740f557bb213791724fdde166965a34f0a49438
Maxim <entorus@yandex.ru>
2017-01-30 16:46:52 +0900

    -

--- src/calc/Main.java
+++ src/calc/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("calc.fxml"));
         primaryStage.setTitle("Network Calculator");
-        primaryStage.setScene(new Scene(root, 320, 150));
+        primaryStage.setScene(new Scene(root, 320, 200));
         primaryStage.show();
     }
 
